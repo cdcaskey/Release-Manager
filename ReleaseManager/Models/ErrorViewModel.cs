4265e5d3acd2993ff3be916d69e66e201753bc3e
Chris Caskey <cdcaskey@users.noreply.github.com>
2018-07-31 19:26:33 +0100

    Add project files.

---
+++ ReleaseManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReleaseManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
