47fe931965a2d21dc160e791841912d26d38d248
Chris Caskey <cdcaskey@users.noreply.github.com>
2018-08-03 14:27:02 +0100

    > Added product index page. > Fixed the page links on the layout

--- ReleaseManager/Data/ApplicationDbContext.cs
+++ ReleaseManager/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ReleaseManager.Models;
 
 namespace ReleaseManager.Data
 {
@@ -12,5 +13,6 @@ namespace ReleaseManager.Data
             : base(options)
         {
         }
+        public DbSet<ReleaseManager.Models.Product> Product { get; set; }
     }
 }
---
+++ ReleaseManager/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReleaseManager.Data;
+using ReleaseManager.Models;
+using ReleaseManager.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReleaseManager.Pages.Products
+{
+    public class IndexModel : BasePageModel
+    {
+
+        private readonly ApplicationDbContext _context;
+        public override string Title => "Products";
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Products { get;set; }
+
+        public async Task OnGetAsync() => Products = await _context.Product.ToListAsync();
+    }
+}
