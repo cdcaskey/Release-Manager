e69c532b7d9f58e8e3b55cd0fb4be29912799d1c
Chris Caskey <cdcaskey@users.noreply.github.com>
2018-08-03 14:55:05 +0100

    > Added the details page. > Removed the ViewData references in the create/edit pages.

---
+++ ReleaseManager/Pages/Products/Details.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReleaseManager.Data;
+using ReleaseManager.Models;
+using ReleaseManager.ViewModels;
+using System;
+using System.Threading.Tasks;
+
+namespace ReleaseManager.Pages.Products
+{
+    public class DetailsModel : BasePageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public override string Title => "Product Details";
+
+        public DetailsModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Product = await _context.Product.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Product == null)
+                return NotFound();
+
+            return Page();
+        }
+    }
+}
