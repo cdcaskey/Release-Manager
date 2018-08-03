7ef4f6c6d4d878d784cccab7d93fedacada6d1f0
Chris Caskey <cdcaskey@users.noreply.github.com>
2018-08-03 14:43:20 +0100

    > Added link to products in the layout page. > Added the product create page.

---
+++ ReleaseManager/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReleaseManager.Models;
+using ReleaseManager.ViewModels;
+using System.Threading.Tasks;
+
+namespace ReleaseManager.Pages.Products
+{
+    public class CreateModel : BasePageModel
+    {
+        private readonly ReleaseManager.Data.ApplicationDbContext _context;
+        public override string Title => "Create Product";
+
+        public CreateModel(ReleaseManager.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            _context.Product.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
